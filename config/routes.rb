20435c6c7bedc2c882f5cd7b2e1fb25f7964ef33
Ben <ben.challenor@btopenworld.com>
2018-06-20 14:17:06 +0100

    Add item controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  resources :items
   devise_for :users, comntrollers: {
     registrations: 'registrations'
   }
   # get 'store/index'
-root 'store#index'
+root 'items#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180619133429) do
+ActiveRecord::Schema.define(version: 20180620131207) do
+
+  create_table "items", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "condition"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
