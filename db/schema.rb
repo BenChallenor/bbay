5c69f5b352944cc16562c4f9e5d23e8c341adb74
Ben <ben.challenor@btopenworld.com>
2018-06-20 15:22:02 +0100

    Add ability to upload an image using carrierwave gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180620131207) do
+ActiveRecord::Schema.define(version: 20180620132504) do
 
   create_table "items", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20180620131207) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
