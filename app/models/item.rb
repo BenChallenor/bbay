0df919e4b1925607e9450199913606f3e570db08
Ben <ben.challenor@btopenworld.com>
2018-06-22 16:47:55 +0100

    Add cart and line item models

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,11 +2,12 @@ class Item < ApplicationRecord
   mount_uploader :image, ImageUploader
   serialize :image, JSON
   belongs_to :user, optional: true
+  has_many :line_items
   validates :title, :price, presence: true
   validates :description, length: { maximum: 1000, too_long: "%{count} characters is the maximum allowed."}
   validates :title, length: { maximum: 140, too_long: "%{count} characters is the maximum allowed."}
   validates :price, numericality: { only_integer: true }, length: { maximum: 7 }
 
   CONDITION = %w{ New Excellent Mint Used Fair Poor }
-  
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180620132504) do
+ActiveRecord::Schema.define(version: 20180622153245) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.string "title"
@@ -21,6 +26,15 @@ ActiveRecord::Schema.define(version: 20180620132504) do
     t.datetime "updated_at", null: false
     t.string "image"
     t.integer "user_id"
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.integer "item_id"
+    t.integer "cart_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_line_items_on_cart_id"
+    t.index ["item_id"], name: "index_line_items_on_item_id"
   end
 
   create_table "users", force: :cascade do |t|
